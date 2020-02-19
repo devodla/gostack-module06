98a777133535a9f01ffcb808c453ad275ca68f09
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-02-19 00:26:30 -0300

    Navigation with Return back

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 
 // import { Container } from './styles';
 
-export default function Main() {
+export default function Main({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Main Screen</Text>
+      <Button title="Go To User" onPress={() => navigation.navigate('User')} />
     </View>
   );
 }
--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 
 // import { Container } from './styles';
 
-export default function User() {
+export default function User({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>User Screen</Text>
+      <Button title="Go To Main" onPress={() => navigation.navigate('Main')} />
+      <Button title="Go Back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
